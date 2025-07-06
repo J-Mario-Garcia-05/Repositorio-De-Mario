32a126cc7e1bbe25f0012a556df9b8aa6c4269ae
unknown <jm.gcaxaj@udeo.edu.gt>
2025-07-06 17:16:18 -0600

    Guardar cambios

--- Veterinaria.py
+++ Veterinaria.py
@@ -1,5 +1,12 @@
-class cliente:
+class Cliente:
     def __init__(self, nombre, telefono, correo):
         self.__nombre = nombre
         self.__telefono = telefono
         self.__correo = correo
+
+class Mascota:
+    def __init__(self, nombre, especie, raza, edad):
+        self.__nombre = nombre
+        self.__especie = especie
+        self.__raza = raza
+        self.__edad = edad
