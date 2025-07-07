379084079e5b5c080638101087604608de9c2439
unknown <jm.gcaxaj@udeo.edu.gt>
2025-07-06 20:21:09 -0600

    Crear case 1, case 2

--- Veterinaria.py
+++ Veterinaria.py
@@ -1,18 +1,3 @@
-class Cliente:
-    def __init__(self, nombre, telefono, correo):
-        self.nombre = nombre
-        self.telefono = telefono
-        self.correo = correo
-        self.mascotas = []
-    def agregar_mascota(self, mascota):
-        self.mascotas.append(mascota)
-    def mostrar_informacion(self):
-        print(f"\nNombre: {self.nombre}, teléfono: {self.telefono}, correo: {self.correo}")
-        if self.mascotas:
-            print("Mascotas:")
-            for i in self.mascotas:
-                i.mostrar_informacion()
-
 class Mascota:
     def __init__(self, nombre, especie, raza, edad):
         self.nombre = nombre
@@ -26,6 +11,57 @@ class Mascota:
     def edad(self, edad):
         if 0 <= edad <= 12:
             self.edad = edad
-    def mostrar_informacion(self):
+    def mostrar_info_mascota(self):
         print(f"\tNombre: {self.nombre}, especie: {self.especie}, raza: {self.raza}, edad: {self.__edad}")
 
+class Cliente:
+    def __init__(self, nombre, telefono, correo):
+        self.nombre = nombre
+        self.telefono = telefono
+        self.correo = correo
+        self.mascotas = []
+    def agregar_mascota(self, mascota):
+        self.mascotas.append(mascota)
+    def mostrar_info_cliente(self):
+        print(f"\nNombre: {self.nombre}, teléfono: {self.telefono}, correo: {self.correo}")
+        if self.mascotas:
+            print("Mascotas:")
+            for i in self.mascotas:
+                i.mostrar_info_mascota()
+
+print("INICIAR SESIÓN:")
+while True:
+    usuario = input("Ingrese el usuario: ")
+    password = input("Ingrese la contraseña: ")
+    if usuario == "admin" and password == "admin":
+        opcion = "0"
+        lista_clientes = []
+        while opcion != "6":
+            print("==SISTEMA VETERINARIA==")
+            print("1.Registrar cliente")
+            print("2.Registrar mascota")
+            print("3.Agendar cita médica")
+            print("4.Ver historial de citas")
+            print("5.Ver clientes y mascotas")
+            print("6.Salir")
+            try:
+                opcion = input("\nSeleccione una opción: ")
+                match opcion:
+                    case "1":
+                        nombre_cliente = input("Ingrese el nombre: ")
+                        telefono = int(input("Ingrese un número de telefono: "))
+                        correo = input("Ingrese un correo: ")
+                        nuevo_cliente = Cliente(nombre_cliente, telefono, correo)
+                        lista_clientes.append(nuevo_cliente)
+                        print("Se ha completado el registro")
+                    case "2":
+                        nombre_mascota = input("Ingrese el nombre de la mascota: ")
+                        especie = input("Ingrese la especie: ")
+                        raza = input("Ingrese la raza: ")
+                        edad = int(input("Ingrese la edad: "))
+                        nueva_masota = Mascota(nombre_mascota, especie, raza, edad)
+                        print("Se ha completado el registro")
+            except ValueError:
+                print("ERROR: Ha ingresado un dato no válido")
+    else:
+        print("Credenciales no válidas vuelva a intentar.")
